73f4972e8bb43c87aa40b5180bc0be82fcb3cd73
CJ Gilroy <cjgilroy@MacBook-Pro.local>
2019-02-12 06:16:09 -0700

    Fix DOMStyle style attribute name errors

--- src/RinkMap.js
+++ src/RinkMap.js
@@ -33,28 +33,28 @@ export class RinkMap extends React.Component {
         let toolTip = '';
         if (shotData.type === "SHOT") {
           element = (
-            <g>
-              <line x1={shotData.coordinates.x - 1.4} y1={shotData.coordinates.y + 1.4} x2={shotData.coordinates.x + 1.4} y2={shotData.coordinates.y - 1.4} stroke-width="0.6" stroke={shotData.color}></line>
-              <line x1={shotData.coordinates.x - 1.4} y1={shotData.coordinates.y - 1.4} x2={shotData.coordinates.x + 1.4} y2={shotData.coordinates.y + 1.4} stroke-width="0.6" stroke={shotData.color}></line>
-              <circle  data-tip data-for={"circle"+i} fill-opacity='0' cx={shotData.coordinates.x} cy={shotData.coordinates.y} r="1.4"></circle>
+            <g key={"element"+i}>
+              <line x1={shotData.coordinates.x - 1.4} y1={shotData.coordinates.y + 1.4} x2={shotData.coordinates.x + 1.4} y2={shotData.coordinates.y - 1.4} strokeWidth="0.6" stroke={shotData.color}></line>
+              <line x1={shotData.coordinates.x - 1.4} y1={shotData.coordinates.y - 1.4} x2={shotData.coordinates.x + 1.4} y2={shotData.coordinates.y + 1.4} strokeWidth="0.6" stroke={shotData.color}></line>
+              <circle  data-tip data-for={"circle"+i} fillOpacity='0' cx={shotData.coordinates.x} cy={shotData.coordinates.y} r="1.4"></circle>
             </g>
           );
           toolTip = (
-            <ReactTooltip id={"circle"+i}>
-              <span class="tooltipTime">{shotData.time}</span>
-              <span class="tooltipInfo">{shotData.player} shot on goal</span>
+            <ReactTooltip key={"tooltip"+i} id={"circle"+i}>
+              <span className="tooltipTime">{shotData.time}</span>
+              <span className="tooltipInfo">{shotData.player} shot on goal</span>
             </ReactTooltip>
           );
         } else {
           element = (
-            <g>
-              <circle data-tip data-for={"circle"+i} cx={shotData.coordinates.x} cy={shotData.coordinates.y} r="1.4" fill={shotData.color} stroke='#fff' stroke-width="0.6"></circle>
+            <g key={"element"+i}>
+              <circle data-tip data-for={"circle"+i} cx={shotData.coordinates.x} cy={shotData.coordinates.y} r="1.4" fill={shotData.color} stroke='#fff' strokeWidth="0.6"></circle>
             </g>
           );
           toolTip = (
-            <ReactTooltip id={"circle"+i}>
-              <span class="tooltipTime">{shotData.time}</span>
-              <span class="tooltipInfo">{shotData.description}</span>
+            <ReactTooltip key={"tooltip"+i} id={"circle"+i}>
+              <span className="tooltipTime">{shotData.time}</span>
+              <span className="tooltipInfo">{shotData.description}</span>
             </ReactTooltip>
           );
         }
@@ -110,7 +110,7 @@ componentDidUpdate() {
             <circle fill="none" cx="0" cy="0" r="15px"/>
           </clipPath>
           <g>
-            <image xlinkHref={this.props.homeResources.imagePath} clip-path="url(#logoCircle)" height="30px" width="30px" opacity="0.5" x='-15' y='-15' borderRadius='15px' ></image>
+            <image xlinkHref={this.props.homeResources.imagePath} clipPath="url(#logoCircle)" height="30px" width="30px" opacity="0.5" x='-15' y='-15'></image>
           </g>
           {output.shotsArray}
         </svg>
@@ -122,42 +122,42 @@ componentDidUpdate() {
 
 function MapLegend(props) {
   return (
-    <div class="legendWrapper">
-      <div class="legendHalf">
-        <span class="legendTricode">{props.homeTricode}</span>
+    <div className="legendWrapper">
+      <div className="legendHalf">
+        <span className="legendTricode">{props.homeTricode}</span>
         <svg viewBox="0 0 2 2" width="16" height="16" xmlns="http://www.w3.org/2000/svg">
-          <circle cx="1" cy="1" r="0.7" fill={props.homeColor} stroke="#fff" stroke-width="0.3"></circle>
+          <circle cx="1" cy="1" r="0.7" fill={props.homeColor} stroke="#fff" strokeWidth="0.3"></circle>
         </svg>
-        <span class="legendText">
+        <span className="legendText">
           <span>Goal</span>
         </span>
         <svg viewBox="0 0 2 2" width="16" height="16" xmlns="http://www.w3.org/2000/svg">
           <g>
-            <circle cx="1" cy="1" r="0.7" fill-opacity="0"></circle>
-            <line x1="0.30000000000000004" y1="0.30000000000000004" x2="1.7" y2="1.7" stroke-width="0.3" stroke={props.homeColor}></line>
-            <line x1="0.30000000000000004" y1="1.7" x2="1.7" y2="0.30000000000000004" stroke-width="0.3" stroke={props.homeColor}></line>
+            <circle cx="1" cy="1" r="0.7" fillOpacity="0"></circle>
+            <line x1="0.30000000000000004" y1="0.30000000000000004" x2="1.7" y2="1.7" strokeWidth="0.3" stroke={props.homeColor}></line>
+            <line x1="0.30000000000000004" y1="1.7" x2="1.7" y2="0.30000000000000004" strokeWidth="0.3" stroke={props.homeColor}></line>
           </g>
         </svg>
-        <span class="legendText">
+        <span className="legendText">
           <span>Shot on Goal</span>
         </span>
       </div>
-      <div class="legendHalf">
-        <span class="legendTricode">{props.awayTricode}</span>
+      <div className="legendHalf">
+        <span className="legendTricode">{props.awayTricode}</span>
         <svg viewBox="0 0 2 2" width="16" height="16" xmlns="http://www.w3.org/2000/svg">
-          <circle cx="1" cy="1" r="0.7" fill={props.awayColor} stroke="#fff" stroke-width="0.3"></circle>
+          <circle cx="1" cy="1" r="0.7" fill={props.awayColor} stroke="#fff" strokeWidth="0.3"></circle>
         </svg>
-        <span class="legendText">
+        <span className="legendText">
           <span>Goal</span>
         </span>
         <svg viewBox="0 0 2 2" width="16" height="16" xmlns="http://www.w3.org/2000/svg">
           <g>
-            <circle cx="1" cy="1" r="0.7" fill-opacity="0"></circle>
-            <line x1="0.30000000000000004" y1="0.30000000000000004" x2="1.7" y2="1.7" stroke-width="0.3" stroke={props.awayColor}></line>
-            <line x1="0.30000000000000004" y1="1.7" x2="1.7" y2="0.30000000000000004" stroke-width="0.3" stroke={props.awayColor}></line>
+            <circle cx="1" cy="1" r="0.7" fillOpacity="0"></circle>
+            <line x1="0.30000000000000004" y1="0.30000000000000004" x2="1.7" y2="1.7" strokeWidth="0.3" stroke={props.awayColor}></line>
+            <line x1="0.30000000000000004" y1="1.7" x2="1.7" y2="0.30000000000000004" strokeWidth="0.3" stroke={props.awayColor}></line>
           </g>
         </svg>
-        <span class="legendText">
+        <span className="legendText">
           <span>Shot on Goal</span>
         </span>
       </div>
