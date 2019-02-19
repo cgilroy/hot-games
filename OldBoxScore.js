6c0e10fc58da614f08aba7d35712327ad6c71017
CJ Gilroy <cjgilroy@MacBook-Pro.local>
2019-02-19 06:13:11 -0700

    Changing boxscore table and some minor CSS

--- OldBoxScore.js
+++ OldBoxScore.js
@@ -29,18 +29,18 @@ export class BoxScoreStateless extends React.Component {
             name: firstInitial + '. ' + lastName,
             goals: skaterStats.goals,
             assists: skaterStats.assists,
-            // points: +skaterStats.goals + +skaterStats.assists,
+            points: +skaterStats.goals + +skaterStats.assists,
             plusMinus: skaterStats.plusMinus,
-            // shots: skaterStats.shots,
-            // penaltyMinutes: skaterStats.penaltyMinutes,
-            // hits: skaterStats.hits,
-            // blocked: skaterStats.blocked,
-            // giveaways: skaterStats.giveaways,
-            // takeaways: skaterStats.takeaways,
-            // faceOffPct: +skaterStats.faceOffWins / +skaterStats.faceoffTaken,
+            shots: skaterStats.shots,
+            penaltyMinutes: skaterStats.penaltyMinutes,
+            hits: skaterStats.hits,
+            blocked: skaterStats.blocked,
+            giveaways: skaterStats.giveaways,
+            takeaways: skaterStats.takeaways,
+            faceOffPct: +skaterStats.faceOffWins / +skaterStats.faceoffTaken,
             timeOnIce: skaterStats.timeOnIce,
-            // powerPlayTimeOnIce: skaterStats.powerPlayTimeOnIce,
-            // shortHandedTimeOnIce: skaterStats.shortHandedTimeOnIce
+            powerPlayTimeOnIce: skaterStats.powerPlayTimeOnIce,
+            shortHandedTimeOnIce: skaterStats.shortHandedTimeOnIce
 
           };
           forwardArray.push(currentForwardArray);
@@ -53,18 +53,18 @@ export class BoxScoreStateless extends React.Component {
             name: firstInitial + '. ' + lastName,
             goals: skaterStats.goals,
             assists: skaterStats.assists,
-            // points: +skaterStats.goals + +skaterStats.assists,
+            points: +skaterStats.goals + +skaterStats.assists,
             plusMinus: skaterStats.plusMinus,
-            // shots: skaterStats.shots,
-            // penaltyMinutes: skaterStats.penaltyMinutes,
-            // hits: skaterStats.hits,
-            // blocked: skaterStats.blocked,
-            // giveaways: skaterStats.giveaways,
-            // takeaways: skaterStats.takeaways,
-            // faceOffPct: +skaterStats.faceOffWins / +skaterStats.faceoffTaken,
+            shots: skaterStats.shots,
+            penaltyMinutes: skaterStats.penaltyMinutes,
+            hits: skaterStats.hits,
+            blocked: skaterStats.blocked,
+            giveaways: skaterStats.giveaways,
+            takeaways: skaterStats.takeaways,
+            faceOffPct: +skaterStats.faceOffWins / +skaterStats.faceoffTaken,
             timeOnIce: skaterStats.timeOnIce,
-            // powerPlayTimeOnIce: skaterStats.powerPlayTimeOnIce,
-            // shortHandedTimeOnIce: skaterStats.shortHandedTimeOnIce
+            powerPlayTimeOnIce: skaterStats.powerPlayTimeOnIce,
+            shortHandedTimeOnIce: skaterStats.shortHandedTimeOnIce
           };
           defenseArray.push(currentDefenseArray);
         } else if (playerType === 'Goalie') {
@@ -106,13 +106,104 @@ export class BoxScoreStateless extends React.Component {
       goalieArray = array[2];
     }
 
-    const forwardColumns = ['Forwards','G','A','+/-','TOI'];
-    const defenseColumns = ['Defense','G','A','+/-','TOI'];
-    const goalieColumns = ['Goalies','SA','SV','SV%','TOI'];
+
+    const forwardColumns = [{
+      Header: 'Forwards',
+      accessor: 'name',
+      maxWidth:140
+    },{
+      Header: 'G',
+      accessor: 'goals',
+      width:30
+    },{
+      Header: 'A',
+      accessor: 'assists',
+      width:30
+    },{
+      Header: 'P',
+      accessor: 'points',
+      width:30
+    },{
+      Header: '+/-',
+      accessor: 'plusMinus',
+      width:30
+    },{
+      Header: 'SOG',
+      accessor: 'shots',
+      width:40
+    },{
+      Header: 'PIM',
+      accessor: 'penaltyMinutes',
+      width:40
+    },{
+      Header: 'HITS',
+      accessor: 'hits',
+      width:50
+    },{
+      Header: 'TOI',
+      accessor: 'timeOnIce',
+      width:60
+    }];
+    const defenseColumns = [{
+      Header: 'Defense',
+      accessor: 'name',
+      width:140// String-based value accessors!
+    },{
+      Header: 'G',
+      accessor: 'goals',
+      width:30
+    },{
+      Header: 'A',
+      accessor: 'assists',
+      width:30
+    },{
+      Header: 'P',
+      accessor: 'points',
+      width:30
+    },{
+      Header: '+/-',
+      accessor: 'plusMinus',
+      width:30
+    },{
+      Header: 'SOG',
+      accessor: 'shots',
+      width:40
+    },{
+      Header: 'PIM',
+      accessor: 'penaltyMinutes',
+      width:40
+    },{
+      Header: 'HITS',
+      accessor: 'hits',
+      width:50
+    },{
+      Header: 'TOI',
+      accessor: 'timeOnIce',
+      width:60
+    }];
+    const goalieColumns = [{
+      Header: 'Goalies',
+      accessor: 'name',
+      width:140// String-based value accessors!
+    },{
+      Header: 'SHOTS',
+      accessor: 'shots',
+      width:80
+    },{
+      Header: 'SAVES',
+      accessor: 'saves',
+      width:80
+    },{
+      Header: 'SV%',
+      accessor: 'savePercentage',
+      width:80
+    },{
+      Header: 'TOI',
+      accessor: 'timeOnIce',
+      width:80
+    }];
 
     let x = makeTable(forwardColumns,forwardArray);
-    let y = makeTable(defenseColumns,defenseArray);
-    let z = makeTable(goalieColumns,goalieArray);
     return(
       <div className="boxScore">
         <div className="buttonRow">
@@ -120,8 +211,33 @@ export class BoxScoreStateless extends React.Component {
           <button className={this.props.activeBoxTeam === 'away' ? 'active' : ''} onClick={()=>this.props.onClick('away')}>{this.props.awayTeamName}</button>
         </div>
         {x}
-        {y}
-        {z}
+        <ReactTable
+          data={forwardArray}
+          columns={forwardColumns}
+          resizable={false}
+          sortable={false}
+          noDataText="No stats available"
+          minRows={0}
+          showPaginationBottom={false}
+        />
+        <ReactTable
+          data={defenseArray}
+          columns={defenseColumns}
+          resizable={false}
+          sortable={false}
+          noDataText="No stats available"
+          minRows={0}
+          showPaginationBottom={false}
+        />
+        <ReactTable
+          data={goalieArray}
+          columns={goalieColumns}
+          resizable={false}
+          sortable={false}
+          noDataText="No stats available"
+          minRows={0}
+          showPaginationBottom={false}
+        />
       </div>
     )
   }
@@ -142,16 +258,11 @@ function makeTable ( headers, data ) {
     return (<tr>{rowData}</tr>)
   }
 
-  let headerRow = headers.map(title => {
-
-    return (<th>{title}</th>)
-  })
-
   let tableData = data.map(row => {
     return tableRow(row);
   });
 
-  return (<table><thead><tr>{headerRow}</tr></thead><tbody>{tableData}</tbody></table>);
+  return (<table>{tableData}</table>);
 
     // // Check type
     // if ( typeof data !== 'object' ) return false;
